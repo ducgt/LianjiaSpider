925a0f00b31e3cdc2adfd3ce6117b7bf164a9a77
zjhdota <zjhdota@outlook.com>
2017-10-31 14:27:35 +0800

    Initial commit

--- src/LianjiaSpider/settings.py
+++ src/LianjiaSpider/settings.py
@@ -28,7 +28,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -59,8 +59,8 @@ COOKIES_ENABLED = False
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware':None,
     'LianjiaSpider.useragent_middleware.RotateUserAgentMiddleware':100,
-    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
-    'LianjiaSpider.proxy_middleware.ProxyMiddleware': 100,
+    #'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+    #'LianjiaSpider.proxy_middleware.ProxyMiddleware': 100,
 }
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
